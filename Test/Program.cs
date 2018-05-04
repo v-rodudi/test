90bbdebb2a69b868e810b75bbc60f68ad5a980fe
v-rodudi <v-rodudi@microsoft.com>
2018-05-04 11:28:11 +0300

    Added hello!

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,7 +10,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello from 'my-branch'!");
+            Console.Write("Hello from master!");
         }
     }
 }
